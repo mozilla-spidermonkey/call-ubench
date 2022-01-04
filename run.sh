04ed52253cb05df1828d96cebea78b1a4526a2eb
Lars T Hansen <lhansen@mozilla.com>
2022-01-04 12:21:58 +0100

    Tweak the runner

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #
 # Note that DEBUG shells emit debugging code into the machine code and
 # even wasm-bound programs will be slower than in non-DEBUG shells.
-SHELL=$HOME/m-u/obj-release/dist/bin/js 
+SHELL="numactl --cpunodebind 1 $HOME/m-u/obj-release/dist/bin/js"
 OPTIONS=--wasm-compiler=ion
 for testcase in *.js; do
     $SHELL $OPTIONS $testcase
